852bf7846e82ebdd8553c46ea4337140c6387a61
BigZhenjie <1741zhenjie@gmail.com>
2025-03-04 15:58:04 -0800

    read/write works

--- fuse_simple.c
+++ fuse_simple.c
@@ -14,6 +14,7 @@
 #include <errno.h>
 #include <sys/time.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 #ifdef HAVE_SETXATTR
 #include <sys/xattr.h>
 #endif
@@ -25,11 +26,27 @@ struct root_dir{
 };
 #define ROOT_DIR ((struct root_dir *) fuse_get_context()->private_data)
 
+
+
 static void fullpath(char fpath[PATH_MAX], const char *path)
 {
     strcpy(fpath, ROOT_DIR->rootdir);
     strncat(fpath, path, PATH_MAX); // ridiculously long paths will
                     // break here
+}
+
+//need this
+static int xmp_getattr(const char *path, struct stat *stbuf)
+{
+    int res;
+    char fpath[PATH_MAX];
+    fullpath(fpath, path);
+
+    res = lstat(fpath, stbuf);
+    if (res == -1)
+        return -errno;
+
+    return 0;
 }
 
 //----------------------------------KEEP----------------------------------
@@ -93,8 +110,13 @@ static int xmp_rmdir(const char *path)
 static int xmp_rename(const char *from, const char *to)
 {
     int res;
+    char fpath_from[PATH_MAX];
+    char fpath_to[PATH_MAX];
+    
+    fullpath(fpath_from, from);
+    fullpath(fpath_to, to);
 
-    res = rename(from, to);
+    res = rename(fpath_from, fpath_to);
     if (res == -1)
         return -errno;
 
@@ -217,18 +239,87 @@ static int xmp_unlink(const char *path)
     return 0;
 };
 
+static int xmp_create(const char *path, mode_t mode, struct fuse_file_info *fi)
+{
+    int fd;
+    char fpath[PATH_MAX];
+    fullpath(fpath, path);
+
+    fd = open(fpath, O_CREAT | O_WRONLY | O_TRUNC, mode);
+    if (fd == -1)
+        return -errno;
+
+    close(fd);
+    return 0;
+}
+
+static int xmp_utimens(const char *path, const struct timespec ts[2])
+{
+    int res;
+    char fpath[PATH_MAX];
+    fullpath(fpath, path);
+
+    /* Don't use utimensat since it might not be available everywhere */
+    struct timeval tv[2];
+    tv[0].tv_sec = ts[0].tv_sec;
+    tv[0].tv_usec = ts[0].tv_nsec / 1000;
+    tv[1].tv_sec = ts[1].tv_sec;
+    tv[1].tv_usec = ts[1].tv_nsec / 1000;
+
+    res = lutimes(fpath, tv);
+    if (res == -1)
+        return -errno;
+
+    return 0;
+}
+
+static int xmp_release(const char *path, struct fuse_file_info *fi)
+{
+    // Nothing to do since we don't keep files open
+    (void) path;
+    (void) fi;
+    return 0;
+}
+
+static int xmp_append(const char *path, const char *buf, size_t size,
+                     struct fuse_file_info *fi)
+{
+    int fd;
+    int res;
+    char fpath[PATH_MAX];
+    
+    // Always open the file for append
+    fullpath(fpath, path);
+    fd = open(fpath, O_WRONLY | O_APPEND);
+    if (fd == -1)
+        return -errno;
+    
+    // Write at the end (O_APPEND ensures this)
+    res = write(fd, buf, size);
+    if (res == -1)
+        res = -errno;
+    
+    // Always close since we opened it here
+    close(fd);
+    return res;
+}
+
 static struct fuse_operations xmp_oper = {
-    .readdir	= xmp_readdir,
-    .mkdir	= xmp_mkdir,
-    .rmdir	= xmp_rmdir,
-    .rename	= xmp_rename,
-    .chmod	= xmp_chmod,
-    .chown	= xmp_chown,
-    .truncate	= xmp_truncate,
-    .open	= xmp_open,
-    .read	= xmp_read,
-    .write	= xmp_write,
+    .getattr    = xmp_getattr,    // Most important!
+    .readdir    = xmp_readdir,
+    .mkdir      = xmp_mkdir,
+    .rmdir      = xmp_rmdir,
     .unlink     = xmp_unlink,
+    .rename     = xmp_rename,
+    .chmod      = xmp_chmod,
+    .chown      = xmp_chown,
+    .truncate   = xmp_truncate,
+    .open       = xmp_open,
+    .create     = xmp_create,     // Important for file creation
+    .read       = xmp_read,
+    .write      = xmp_write,
+    .release    = xmp_release,
+    .utimens    = xmp_utimens,    // Important for timestamp updates
 };
 
 
